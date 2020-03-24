3cfec2f8e3e614080da126b071a37ddcfcca511e
JeyDev310 <mtroz310@gmail.com>
2020-03-24 13:06:18 +0700

    template

--- src/index.js
+++ src/index.js
@@ -1,27 +1,36 @@
-import 'core-js/es6/string';
-import 'core-js/es6/array';
-import 'core-js/es6/map';
-import 'core-js/es6/set';
-import 'core-js/es6/object';
-import 'core-js/es6/promise';
-import 'core-js/es7/object';
-import 'core-js/es7/array';
-import 'raf/polyfill';
-
+import './polyfills'
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+import * as serviceWorker from './serviceWorker';
+
+import { HashRouter } from 'react-router-dom';
+import './assets/base.scss';
+import Main from './DemoPages/Main';
+import configureStore from './config/configureStore';
 import { Provider } from 'react-redux';
 
-import App from './App';
-
-import './i18n';
-
-import configureStore from './store/store';
 const store = configureStore();
+const rootElement = document.getElementById('root');
 
-ReactDOM.render(
-    <Provider store={store}>
-        <App />
-    </Provider>,
-    document.getElementById('app')
-);
+const renderApp = Component => {
+    ReactDOM.render(
+        <Provider store={store}>
+            <HashRouter>
+                <Component />
+            </HashRouter>
+        </Provider>,
+        rootElement
+    );
+};
+
+renderApp(Main);
+
+if (module.hot) {
+    module.hot.accept('./DemoPages/Main', () => {
+        const NextApp = require('./DemoPages/Main').default;
+        renderApp(NextApp);
+    });
+}
+serviceWorker.unregister();
+
